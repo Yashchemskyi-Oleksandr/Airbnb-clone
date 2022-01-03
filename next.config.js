1b197a3cd97593c16c8746264218819f39fafa20
Oleksandr Yashchemskyi <yashchemskyio@gmail.com>
2022-01-03 16:02:57 +0200

    done:airbnb-clone

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        domains: ['links.papareact.com'],
+    },
+    env: {
+        mapbox_key:
+            'pk.eyJ1IjoiYWxleHJlYWN0IiwiYSI6ImNreHh0MWY1cDR1ZzkycHVid2NsOXludWYifQ.-HWRYbwb7bgB5U5OLZB6oA',
+    },
+};
